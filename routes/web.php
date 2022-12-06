13ad46799b4b0bb412879687ce6599336a6a2d91
renaldybrada <renaldybrada@gmail.com>
2022-12-06 13:32:52 +0700

    routing example

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/text', function() {
+    return 'it works';
+});
+
+Route::get('/html', function() {
+    return '<h1 style="color:red">It works!</h1>';
+});
+
+Route::get('/picture', function() {
+    return '<img src="https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80">';
+});
